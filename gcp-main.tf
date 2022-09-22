fd990dd4382b76014609ef41cf3a462742c94bc8
ricardogeske <113803562+ricardogeske@users.noreply.github.com>
2022-09-22 19:29:52 -0300

    teste

--- gcp-main.tf
+++ gcp-main.tf
@@ -3,7 +3,6 @@ resource "google_app_engine_application" "app" {
   location_id = "us-central1"
 }
 
-
 resource "google_artifact_registry_repository" "my-repo" {
   location = "us-central1"
   repository_id = "labdevops"
